e0bfadb20add8105774a7e34db94867c47d660ef
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2018-09-21 14:17:30 -0300

    delete nginx para pensado para Debian 9
    Este script esta pensado para resolver algunos problemas generados en mi caso uso Webmin y tuve que crearlo para resolver este problema.
    Simple sencillo y para un destino borrar nginx.
    This script is designed to solve some problems generated in my case I use Webmin and I had to create it to solve this problem.
    Simple simple and for a destination erase nginx.

---
+++ delete.sh
@@ -0,0 +1,40 @@
+echo 'Bienvenido ahora vas a borrar nginx del sistema'
+sudo -v
+echo 'ES:listo has permitido este programa se ejecute
+¿Te queda alguna duda?, puedes consultar este hilo
+EN:ready you have allowed this program to run
+Do you have any questions? You can check this thread
+https://www.howtoinstall.co/en/debian/stretch/nginx?action=remove
+'
+clear;
+echo 'ES:Ahora pasamos a realizar el borrado del nginx
+EN:Now we go to perform the deletion of the nginx'
+sudo apt-get remove nginx
+echo 'ES:removido!, ahora vamos por las dependencias
+EN:removed !, now we go through the dependencies'
+sudo apt-get remove --auto-remove nginx
+echo 'ES:ahora vamos a limpiar el sistema purgando todo
+EN:now let's clean the system purging everything'
+sudo apt-get purge nginx
+echo 'ES:la etapa de borrado extendida
+EN:the extended erase stage'
+sudo apt-get purge --auto-remove nginx
+echo 'ES: removiendo otros sistemas adyacentes de nginx
+EN:removing other adjacent nginx systems'
+sudo apt-get remove nginx-*
+echo 'ES:limpiando el sistema de adyacentes para nginx
+EN:cleaning the adjacent system for nginx'
+sudo apt-get purge nginx-*
+clear;
+echo 'ES: listo tu sistema esta limpio del nginx
+si tienes instalado debian te recomiendo
+pasar el siguiente comando
+systemctl start apache2 y luego
+pon la contraseña root o famosamente
+llamada raiz
+EN:ready your system is clean of the nginx
+if you have installed debian I recommend you
+pass the next command
+systemctl start apache2 and then
+enter the password root or famously
+root call'
